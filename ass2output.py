16e5418f01a5a99288b77fc543a0f7204c67c2cb
Rahul LJ <122854516+Rahul-LJ@users.noreply.github.com>
2023-01-25 18:19:12 +0530

    Delete ass2output.py

--- ass2output.py
+++
@@ -1,8 +0,0 @@
-s1 = "Computer"
-s2 = "Science"
-print(s2*(len(s1)//4))
-print(s1[0]+s2[1],end ="$$")
-print(s2+"is a\n impor\tant" +s1[3]*2)
-print(print(45 > 23> 77))
-print( bool(0) and bool(-99))
-
